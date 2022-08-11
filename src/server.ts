bc4add2d2c0cce0cd84e6111b9b8cefe8802b045
samarkhaled4 <samarkhaled.iti@gmail.com>
2022-08-11 13:49:48 +0200

    editing in server file

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
+import ProductsRoute from './handlers/products'
+import UsersRoute from './handlers/users'
+import OrdersRoute from './handlers/orders'
 
 const app: express.Application = express()
 //const address: string = "0.0.0.0:3000"
@@ -9,6 +12,10 @@ app.use(bodyParser.json())
 app.get('/', function (req: Request, res: Response) {
     res.send('Hello World!')
 })
+
+ProductsRoute(app);
+UsersRoute(app);
+OrdersRoute(app);
 
 app.listen(3000, function () {
     console.log('Server start listening on port: 3000');

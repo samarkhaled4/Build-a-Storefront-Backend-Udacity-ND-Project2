66278d35af7eb5ba9cf47b231483305093fa5707
samarkhaled4 <samarkhaled.iti@gmail.com>
2022-08-28 11:37:08 +0200

    add test for addProduct function

--- src/tests/orderSpec.ts
+++ src/tests/orderSpec.ts
@@ -1,11 +1,12 @@
 import exp from "constants";
 import {Order,OrderStore} from "../models/order"
+import { Product,ProductStore } from "../models/product";
 import {User,UserStore} from "../models/user"
 
 const order=new OrderStore();
-
 const myOrder:Order={id:1,status:'active',user_id:1};
 const user=new UserStore();
+const product=new ProductStore();
 
 describe('Order model test',()=>{
 
@@ -13,14 +14,14 @@ describe('Order model test',()=>{
         expect(order.index).toBeDefined();
     });
     it('should have a show method', () => {
-        expect(order.index).toBeDefined();
+        expect(order.show).toBeDefined();
     });
     it('should have a create method', () => {
-        expect(order.index).toBeDefined();
+        expect(order.create).toBeDefined();
     });
     
     it('should have a delete method', () => {
-        expect(order.index).toBeDefined();
+        expect(order.delete).toBeDefined();
     });
 
     it('create method should add an order', async () => {
@@ -37,6 +38,17 @@ describe('Order model test',()=>{
     it('show method should return the correct order',async()=>{
         const result=await order.show("1");
         expect(result).toEqual(myOrder)
+    });
+
+    it('addProduct method should add product to the order',async()=>{
+        await product.create({pname:'t-shirt',price:50});
+        const result=await order.addProduct(2,'1','1');
+        expect(result).toEqual({
+            id:1,
+            order_id:1,
+            product_id:1,
+            quantity:2
+        })
     });
 
     it('delete method should delete the order',async()=>{

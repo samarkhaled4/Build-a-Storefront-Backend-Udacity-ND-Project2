2cad4823098ce9eb2b1e6cc87344aaccf49e81aa
samarkhaled4 <samarkhaled.iti@gmail.com>
2022-08-28 11:37:58 +0200

    edit to meet changes in testing order

--- src/tests/productSpec.ts
+++ src/tests/productSpec.ts
@@ -2,7 +2,7 @@ import {Product,ProductStore} from "../models/product"
 
 const product=new ProductStore();
 
-const myProduct:Product={id:1,pname:'skirt',price:50}
+const myProduct:Product={id:2,pname:'skirt',price:50}
 
 describe('Product model test',()=>{
   it('should have an index method', () => {
@@ -26,29 +26,36 @@ describe('Product model test',()=>{
   });
   it('index method should return an array of products',async()=>{
     const res=await product.index();
-    expect(res).toEqual([myProduct]);
+    //console.log(res);
+    expect(res).toEqual([
+      {id:1,
+      pname:'t-shirt',
+      price:50},
+      {id:2,
+      pname:'skirt',
+      price:50}]);
   });
   
   it('show method should return the correct product', async () => {
-    const result = await product.show("1");
+    const result = await product.show("2");
     expect(result).toEqual(myProduct);
   });
 
   it('update method should update product',async()=>{
-    const myproduct=await product.show("1");
+    const myproduct=await product.show("2");
     const values={"price":30};
     const updated=await product.update(myproduct,values);
     expect(updated).toEqual({
-      id:1,
+      id:2,
       pname:'skirt',
       price:30
     })
   });
 
   it('delete method should remove the product', async () => {
+    await product.delete("2");
     await product.delete("1");
     const result = await product.index()
     expect(result).toEqual([]);
-  }) 
-  
+  })   
 })
